cbfe20106fbadc249ca439d5cae8b1d46a6d5fba
Cíntia Fumi <cintiafumi@gmail.com>
2021-04-23 23:59:23 -0300

    ♻️  refac: Read user and quizzes json files

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,6 +1,5 @@
+import 'package:devquiz/home/home_repository.dart';
 import 'package:devquiz/home/home_state.dart';
-import 'package:devquiz/shared/models/answer_model.dart';
-import 'package:devquiz/shared/models/question_model.dart';
 import 'package:devquiz/shared/models/quiz_model.dart';
 import 'package:devquiz/shared/models/user_model.dart';
 import 'package:flutter/foundation.dart';
@@ -13,47 +12,17 @@ class HomeController {
   UserModel? user;
   List<QuizModel>? quizzes;
 
+  final repository = HomeRepository();
+
   void getUser() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-    user = UserModel(
-      name: "Cintia Fumi",
-      photoUrl: "https://github.com/cintiafumi.png",
-    );
+    user = await repository.getUser();
     state = HomeState.success;
   }
 
   void getQuizzes() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-    quizzes = [
-      QuizModel(
-        title: "NLW 5 Flutter",
-        image: "",
-        questionAnswered: 1,
-        level: Level.facil,
-        questions: [
-          QuestionModel(
-            title: "Está curtindo o Flutter",
-            answers: [
-              AnswerModel(title: "Estou curtindo"),
-              AnswerModel(title: "Estou amando"),
-              AnswerModel(title: "Top"),
-              AnswerModel(title: "Show de bola", isRight: true),
-            ],
-          ),
-          QuestionModel(
-            title: "Está curtindo o Flutter",
-            answers: [
-              AnswerModel(title: "Estou curtindo"),
-              AnswerModel(title: "Estou amando"),
-              AnswerModel(title: "Top"),
-              AnswerModel(title: "Show de bola", isRight: true),
-            ],
-          ),
-        ],
-      )
-    ];
+    quizzes = await repository.getQuizzes();
     state = HomeState.success;
   }
 }
